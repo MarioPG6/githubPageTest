581df0a3f625bcaedfcead97b354c4bdadd9a642
Mario <mariostteven@gmail.com>
2025-09-18 13:13:18 -0500

    add:diseño

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,7 @@ fetch("./data/data.json")
     renderDocuments(allDocuments);
 
     // --- Insertar filtros ---
-    const fasesOrden = ["iniciación", "ciclo-1", "seguimiento","requerimientos","planeacion"];
+    const fasesOrden = ["iniciación", "ciclo-1", "seguimiento","requerimientos","planeacion","diseño"];
     const filtrosContainer = document.createElement("div");
     filtrosContainer.className = "busqueda-filtros";
 
@@ -114,7 +114,7 @@ function renderDocuments(docs) {
     return;
   }
 
-  const fasesOrden = ["iniciación", "ciclo-1", "seguimiento","requerimientos","planeacion"];
+  const fasesOrden = ["iniciación", "ciclo-1", "seguimiento","requerimientos","planeacion","diseño"];
   const grouped = {};
 
   docs.forEach((doc) => {
